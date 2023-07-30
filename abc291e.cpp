921d9e61bb97b9e1f99d345355e2dcee84343b99
contengee <contengee@gmail.com>
2023-07-30 21:59:20 +0900

    Update abc291e.cpp
    コメント行を追加。
    ソースが分かりにくいので説明用に分かりやすいコードに書き換えた方が良さそう。

--- abc291e.cpp
+++ abc291e.cpp
@@ -12,21 +12,21 @@ int main(){
 		cin >> x >> y;
 		x--,y--;
 		G[x].push_back(y);
-		deg[y]++;
+		deg[y]++; # 入次数をカウント
 	}
 	
 	vector<int>ans(n);
 	queue<int>q;
 	int cnt=0;
-	for(int i=0;i<n;i++)if(deg[i]==0)q.push(i);
+	for(int i=0;i<n;i++)if(deg[i]==0)q.push(i); # 一筆書きの最初の地点を検索
 	while(!q.empty()){
-		if(q.size()!=1){
+		if(q.size()!=1){ # 開始地点が2つ以上ある時は無条件でNo
 			cout << "No" << endl;
 			return 0;
 		}
-		int v=q.front();q.pop();
+		int v=q.front();q.pop(); # qに入っている指し示す頂点vを取り出す
 		ans[v]=++cnt;
-		for(auto vv:G[v])if(!--deg[vv])q.push(vv);
+		for(auto vv:G[v])if(!--deg[vv])q.push(vv); # 入次数が1の時にpush
 	}
 
 	cout << "Yes" << endl;
